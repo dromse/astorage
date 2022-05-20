22be067cb494caeec72094bf4a57a04dc511461e
dromse <dromseproducer@gmail.com>
2022-05-21 01:26:40 +0300

    add middleware to routers

--- server/routes/audioRouter.js
+++ server/routes/audioRouter.js
@@ -1,11 +1,15 @@
 const Router = require('express')
 
 const AudioController = require('../controllers/audioController')
+const RoleMiddleware = require('../middleware/roleMiddleware')
 
 const router = new Router()
 
-router.post('/', AudioController.upload)
-router.get('/:fileName', AudioController.download)
-router.delete('/:fileName', AudioController.remove)
+router.post('/', RoleMiddleware(['USER']), AudioController.upload)
+
+router.get('/getAll', RoleMiddleware(['USER']), AudioController.getAll)
+router.get('/:fileId', RoleMiddleware(['USER']), AudioController.download)
+
+router.delete('/:fileId', RoleMiddleware(['USER']), AudioController.remove)
 
 module.exports = router
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,12 +1,13 @@
 const Router = require('express')
 
 const UserController = require('../controllers/userController')
+const RoleMiddleware = require('../middleware/roleMiddleware')
 
 const router = new Router()
 
 router.post('/signup', UserController.signup)
 router.post('/login', UserController.login)
 router.post('/logout', UserController.logout)
-router.get('/getUsers', UserController.getUsers)
+router.get('/getAll', RoleMiddleware(['USER']), UserController.getAll)
 
 module.exports = router
