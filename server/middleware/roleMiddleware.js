96a8b3df4be0ef8206678c1e7c2c9b1e4799d4d8
dromse <dromseproducer@gmail.com>
2022-05-21 01:22:53 +0300

    add roleMiddleware

---
+++ server/middleware/roleMiddleware.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = function (roles) {
+  return function (req, res, next) {
+    if (req.method === 'OPTIONS') {
+      next()
+    }
+
+    try {
+      const token = req.headers.authorization.split(' ')[1]
+      if (!token) {
+        return res.status(403).json({ message: 'User is not authorized.' })
+      }
+
+      const { role } = jwt.verify(token, process.env.SECRET_KEY)
+      let hasRole = false
+
+      if (roles.includes(role)) {
+        hasRole = true
+      }
+
+      if (!hasRole) {
+        return res.status(403).json({ message: 'You does not have access.' })
+      }
+
+      next()
+    } catch (err) {
+      return res.status(403).json({ message: 'User is not authorized.' })
+    }
+  }
+}
