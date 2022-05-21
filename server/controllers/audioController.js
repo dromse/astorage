f85578b45065c1cce83c3334f30bd08c91ea028d
dromse <dromseproducer@gmail.com>
2022-05-22 00:01:55 +0300

    add change visibility and title

--- server/controllers/audioController.js
+++ server/controllers/audioController.js
@@ -1,11 +1,21 @@
+const ApiError = require('../exceptions/apiError')
 const AudioService = require('../services/audioService')
 
 class AudioController {
   async upload(req, res, next) {
     try {
       const { audio } = req.files
+      const { visibility, title } = req.body
+      const user = req.user
 
-      const pathToFile = await AudioService.upload(audio)
+      const audioInfo = {
+        visibility,
+        userId: user.id,
+        title,
+      }
+
+      const pathToFile = await AudioService.upload(audio, audioInfo)
+
       res.status(200).json({ pathToFile })
     } catch (e) {
       next(e)
@@ -35,12 +45,69 @@ class AudioController {
     }
   }
 
-  async getAll(req, res, next) {
+  async getUserAudios(req, res, next) {
+    try {
+      const user = req.user
+      const audios = await AudioService.getUserAudios(user.id)
+      res.status(200).json({ audios })
+    } catch (e) {
+      next(e)
+    }
+  }
+
+  async getAll(_, res, next) {
     try {
       const audios = await AudioService.getAll()
       res.status(200).json({ audios })
     } catch (e) {
       next(e)
+    }
+  }
+
+  async changeVisibility(req, res, next) {
+    try {
+      const { fileId } = req.params
+      const { visibility } = req.body
+      const user = req.user
+
+      if (!visibility) {
+        next(ApiError.BadRequest('Visibility cannot be empty'))
+      }
+
+      const dataToUpdate = { visibility }
+
+      const audio = await AudioService.updateData(
+        fileId,
+        user.id,
+        dataToUpdate,
+      )
+      res.status(200).json({ audio })
+    } catch (err) {
+      next(err)
+    }
+  }
+
+  async changeTitle(req, res, next) {
+    try {
+      const { fileId } = req.params
+      const { title } = req.body
+      const user = req.user
+
+      if (!title) {
+        next(ApiError.BadRequest('Title cannot be empty'))
+      }
+
+      const dataToUpdate = { title }
+
+      const audio = await AudioService.updateData(
+        fileId,
+        user.id,
+        dataToUpdate,
+      )
+      
+      res.status(200).json({ audio })
+    } catch (err) {
+      next(err)
     }
   }
 }
